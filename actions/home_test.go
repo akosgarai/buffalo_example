00b0fdbb3c105abb8416190c9b0a9d7d07d411eb
akosgarai <szlem01@gmail.com>
2019-03-25 22:35:40 +0100

    #5 - fix / tests

--- actions/home_test.go
+++ actions/home_test.go
@@ -4,27 +4,32 @@ import "github.com/akosgarai/buffalo_example/models"
 
 func (as *ActionSuite) Test_HomeHandler() {
 	res := as.HTML("/").Get()
-	as.Equal(200, res.Code)
-	as.Contains(res.Body.String(), "Sign In")
+	// Should redirect to /login page
+	as.Equal(302, res.Code)
+	as.Contains(res.Body.String(), "<a href=\"/login\">Found</a>")
 }
 
 func (as *ActionSuite) Test_HomeHandler_LoggedIn() {
 	u := &models.Administrator{
-		Email:           "mark@example.com",
-		Pwd:             "password",
-		PwdConfirmation: "password",
+		Email:           "test-admin@admin.com",
+		Pwd:             "admin",
+		PwdConfirmation: "admin",
+		Name:            "test admin",
+		Username:        "testAdmin",
+		Password:        "$2a$10$zm/lRrBz8kObRwHcF1erHOc6Ac2o7Cog.KP8fVen388EtvNaASJHW",
 	}
 	verrs, err := u.Create(as.DB)
 	as.NoError(err)
 	as.False(verrs.HasAny())
-	as.Session.Set("current_user_id", u.ID)
+	as.Session.Set("current_admin_id", u.ID)
 
 	res := as.HTML("/").Get()
 	as.Equal(200, res.Code)
-	as.Contains(res.Body.String(), "Sign Out")
+	as.Contains(res.Body.String(), "logout")
 
 	as.Session.Clear()
+	// Should redirect to /login page
 	res = as.HTML("/").Get()
-	as.Equal(200, res.Code)
-	as.Contains(res.Body.String(), "Sign In")
+	as.Equal(302, res.Code)
+	as.Contains(res.Body.String(), "<a href=\"/login\">Found</a>")
 }
