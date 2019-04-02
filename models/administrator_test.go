0f6788b847107368f75b9dd8f04467b35f742409
akosgarai <szlem01@gmail.com>
2019-04-02 22:26:27 +0200

    #5 - Add testcases for administrator model

--- models/administrator_test.go
+++ models/administrator_test.go
@@ -1,5 +1,23 @@
 package models
 
-func (ms *ModelSuite) Test_Administrators() {
+func (ms *ModelSuite) Test_Administrators_String() {
+	ms.Fail("Not Implemented!")
+}
+func (ms *ModelSuite) Test_Administrator_String() {
+	ms.Fail("Not Implemented!")
+}
+func (ms *ModelSuite) Test_Administrator_Create() {
+	ms.Fail("Not Implemented!")
+}
+func (ms *ModelSuite) Test_Administrator_HasPrivFor() {
+	ms.Fail("Not Implemented!")
+}
+func (ms *ModelSuite) Test_Administrator_Validate() {
+	ms.Fail("Not Implemented!")
+}
+func (ms *ModelSuite) Test_Administrator_ValidateCreate() {
+	ms.Fail("Not Implemented!")
+}
+func (ms *ModelSuite) Test_Administrator_ValidateUpdate() {
 	ms.Fail("Not Implemented!")
 }
